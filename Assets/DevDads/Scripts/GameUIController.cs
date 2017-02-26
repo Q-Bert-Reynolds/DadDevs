b35ba88c42d4768dc2343b65208415b9b1ecdd1c
Yomic <trueyomic@yahoo.com>
2017-02-26 11:52:11 -0600

    * bringing in changes

---
+++ Assets/DevDads/Scripts/GameUIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour {
+
+	public static GameUIController instance;
+
+	public Text scoreText;
+    public Text livesText;
+    public Text gameOverText;
+    public Text energyText;
+
+    void Awake () {
+    	if (instance == null) {
+    		instance = this;
+    	}
+    }
+
+    void Start () {
+        livesText.text = "";
+        gameOverText.text = "";
+        energyText.text = "";
+    }
+
+    public static void ShowGameOver () {
+    	instance.gameOverText.text = "Game Over - Press 'R' to Restart";
+    }
+
+   	public static void UpdateScore(int score) {
+        instance.scoreText.text = "Score: " + score;
+    }
+
+   	public static void UpdateLives(int lives) {
+        instance.livesText.text = "Lives: " + lives;
+    }
+
+   	public static void UpdateEnergy(float energy) {
+        instance.energyText.text = "Energy: " + energy;
+    }
+    
+}
